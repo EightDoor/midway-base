fd345fc0f2d19e274c3f6f9343ae2eef08197024
EightDoor <851708184@qq.com>
2022-04-02 17:18:01 +0800

    init

---
+++ src/config/config.default.ts
@@ -0,0 +1,9 @@
+import { MidwayConfig } from '@midwayjs/core';
+
+export default {
+  // use for cookie sign key, should change to your own and keep security
+  keys: '1648890990968_9932',
+  koa: {
+    port: 7001,
+  },
+} as MidwayConfig;
